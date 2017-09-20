384d6c844d6d9bcdea53fe34abb0c51f4f1f6101
Miguel Rodriguez <angek37@outlook.com>
2017-09-19 20:47:20 -0500

    Crea ventana con OpenGL

---
+++ Ventana.py
@@ -0,0 +1,32 @@
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+import sys
+
+name = 'Grafica'
+
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(400,400)
+    glutCreateWindow(name)
+
+    glutDisplayFunc(display)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(40.,1.,1.,40.)
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(0,0,10,
+              0,0,0,
+              0,1,0)
+    glPushMatrix()
+    glutMainLoop()
+    return
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+    glPopMatrix()
+    glutSwapBuffers()
+    return
+
+if __name__ == '__main__': main()
