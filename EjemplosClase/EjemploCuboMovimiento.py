7a8e54e6c8272232bc2c4af1454720af589b1217
Miguel Rodriguez <angek37@outlook.com>
2017-09-27 12:07:05 -0500

    Corrige clase de cuadro moviendose

--- EjemplosClase/EjemploCuboMovimiento.py
+++ EjemplosClase/EjemploCuboMovimiento.py
@@ -7,34 +7,35 @@ from OpenGL.GLU import *
 
 import numpy as np
 from time import sleep
-	
-x=0.0
-y=0.0
-	
-class Cuad(Wanimation):
-		def AnimationStep(self):
-			global y
-			global x
-			frameRate = 10
-			x += 0.01
-			x = y
-			sleep(l /float( frameRate) )
-			glutPostRedisplay()
-		
-		def display(self):
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            glMatrixMode(GL_MODELVIEW)
-			glLoadIdentity()
-			glColor3f(1,1,1)
-			glTranslatef(x,y,1,0)
-			glBegin( GL_QUADS )
-			glVertex3f( -0.05, 0.05, 0)
-			glVertex3f( -0.05, -0.05, 0)
-			glVertex3f( 0.05, -0.05, 0)
-			glVertex3f( 0.05, 0.05, 0)
-			glEnd()
 
+x = 0.0
+y = 0.0
+l = 0.0
+
+class Cuad(Wanimation):
+    def AnimationStep(self):
+        global y
+        global x
+        frameRate = 10
+        x += 0.01
+        x = y
+        sleep(l / float(frameRate))
+        glutPostRedisplay()
+
+
+    def display(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glColor3f(1, 1, 1)
+        glTranslate(x, y, 1, 0)
+        glBegin(GL_QUADS)
+        glVertex3f(-0.05, 0.05, 0)
+        glVertex3f(-0.05, -0.05, 0)
+        glVertex3f(0.05, -0.05, 0)
+        glVertex3f(0.05, 0.05, 0)
+        glEnd()
 
 if __name__ == '__main___':
     win = Cuad()
-    win.run('cuad',520,520)
+    win.run('cuad', 520, 520)
